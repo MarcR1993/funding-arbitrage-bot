646a38ceea0bfa0ffc154575fdc3a7203cd60476
MarcR1993 <marc.rugel@gmail.com>
2025-06-14 21:40:01 +0200

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,4 +2,72 @@
 Models package for Funding Arbitrage Bot
 """
 
-from .position import Position, PositionStatus, ExchangePosition, FundingMetrics
+# Position models
+from .position import (
+    Position, 
+    PositionStatus, 
+    PositionSide,
+    ExchangePosition, 
+    FundingMetrics
+)
+
+# Opportunity models
+from .opportunity import (
+    ArbitrageOpportunity,
+    OpportunityType,
+    OpportunityPriority,
+    ExchangeData,
+    OpportunityFilter,
+    create_opportunity
+)
+
+# Configuration models
+from .config import (
+    FundingBotConfig,
+    BotConfig,
+    TradingConfig,
+    RiskManagementConfig,
+    ExchangesConfig,
+    MonitoringConfig,
+    BotMode,
+    LogLevel,
+    create_config,
+    load_config_from_env
+)
+
+# Exchange models
+from .exchange import (
+    FundingRate,
+    MarketData,
+    OrderBook,
+    Order,
+    Trade,
+    ExchangeInfo,
+    ExchangeStatus,
+    ExchangeBalance,
+    OrderSide,
+    OrderType,
+    OrderStatus,
+    create_exchange_info,
+    calculate_funding_arbitrage_spread
+)
+
+__all__ = [
+    # Position
+    'Position', 'PositionStatus', 'PositionSide', 'ExchangePosition', 'FundingMetrics',
+    
+    # Opportunity
+    'ArbitrageOpportunity', 'OpportunityType', 'OpportunityPriority', 
+    'ExchangeData', 'OpportunityFilter', 'create_opportunity',
+    
+    # Config
+    'FundingBotConfig', 'BotConfig', 'TradingConfig', 'RiskManagementConfig',
+    'ExchangesConfig', 'MonitoringConfig', 'BotMode', 'LogLevel', 
+    'create_config', 'load_config_from_env',
+    
+    # Exchange
+    'FundingRate', 'MarketData', 'OrderBook', 'Order', 'Trade',
+    'ExchangeInfo', 'ExchangeStatus', 'ExchangeBalance',
+    'OrderSide', 'OrderType', 'OrderStatus',
+    'create_exchange_info', 'calculate_funding_arbitrage_spread'
+]
