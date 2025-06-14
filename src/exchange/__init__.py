d63a654271722577a622bedfa1043459c0409793
MarcR1993 <marc.rugel@gmail.com>
2025-06-14 21:58:27 +0200

    Update __init__.py

--- src/exchange/__init__.py
+++ src/exchange/__init__.py
@@ -2,4 +2,27 @@
 Exchange Connectors Package
 """
 
-from .base_connector import BaseExchangeConnector, ExchangeError, ConnectionError, TradingError
+from .base_connector import (
+    BaseExchangeConnector, 
+    ExchangeError, 
+    ConnectionError, 
+    TradingError,
+    InsufficientBalanceError,
+    RateLimitError
+)
+
+from .binance_connector import BinanceConnector
+from .kucoin_connector import KuCoinConnector  
+from .hyperliquid_connector import HyperliquidConnector
+
+__all__ = [
+    'BaseExchangeConnector',
+    'ExchangeError',
+    'ConnectionError', 
+    'TradingError',
+    'InsufficientBalanceError',
+    'RateLimitError',
+    'BinanceConnector',
+    'KuCoinConnector',
+    'HyperliquidConnector'
+]
