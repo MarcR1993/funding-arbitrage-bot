dc0bf69bf41d63fdfe4eb96df2d238e277ff2232
MarcR1993 <marc.rugel@gmail.com>
2025-06-15 14:40:06 +0200

    Update __init__.py

--- src/connectors/__init__.py
+++ src/connectors/__init__.py
@@ -1,1 +1,13 @@
+from .base_connector import BaseConnector, ConnectorStatus
+from .binance_connector import BinanceConnector
+from .bybit_connector import BybitConnector
+from .hyperliquid_connector import HyperliquidConnector
+from .kucoin_connector import KuCoinConnector
+from .connector_manager import ConnectorManager
 
+__all__ = [
+    "BaseConnector", "ConnectorStatus",
+    "BinanceConnector", "BybitConnector", 
+    "HyperliquidConnector", "KuCoinConnector",
+    "ConnectorManager"
+]
