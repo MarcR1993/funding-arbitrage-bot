2a8c1cdfe2a166367dc62f40bc4f71f95b77fa10
MarcR1993 <marc.rugel@gmail.com>
2025-06-15 15:04:11 +0200

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,16 @@
+from .order import Order, OrderStatus, OrderType, OrderSide
+from .balance import Balance
+from .funding_rate import FundingRate
+from .position import Position, PositionSideAdd commentMore actions
 """
 Trading strategies for funding rate arbitrage.
 """
 
+__all__ = [
+    "Order", "OrderStatus", "OrderType", "OrderSide",
+    "Balance", "FundingRate", 
+    "Position", "PositionSide"
+]
 from .funding_arbitrage import FundingRateArbitrage
 
 __all__ = ["FundingRateArbitrage"]
