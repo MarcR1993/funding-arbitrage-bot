548fa650c8ab4e08c77a4a7a75668606295b16cd
MarcR1993 <marc.rugel@gmail.com>
2025-06-15 14:39:18 +0200

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,16 +1,10 @@
-"""
-Data models for the funding rate arbitrage bot.
-Attribution: Structure inspired by Hummingbot's data models (Apache 2.0)
-"""
-
 from .order import Order, OrderStatus, OrderType, OrderSide
 from .balance import Balance
 from .funding_rate import FundingRate
-from .position import Position
+from .position import Position, PositionSide
 
 __all__ = [
     "Order", "OrderStatus", "OrderType", "OrderSide",
-    "Balance", 
-    "FundingRate",
-    "Position"
+    "Balance", "FundingRate", 
+    "Position", "PositionSide"
 ]
