05fa2e2cae76e328a92ec82f75add5024a5a9dd2
MarcR1993 <marc.rugel@gmail.com>
2025-06-15 14:56:33 +0200

    Create __init__.py

---
+++ src/controllers/__init__.py
@@ -0,0 +1,8 @@
+"""
+V2 Strategy Controllers.
+Attribution: Based on Hummingbot's Controller framework (Apache 2.0)
+"""
+
+from .funding_arbitrage_controller import FundingArbitrageController
+
+__all__ = ["FundingArbitrageController"]
